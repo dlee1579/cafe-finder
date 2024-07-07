e28bc9887ea25ec2017058c3233ab795fc75b5bf
D Lee <dlee1579@gmail.com>
2024-07-07 16:08:42 -0400

    added aggregate ratings to cafe serializer

--- cafe/api/serializers.py
+++ cafe/api/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 from cafe.models import Cafe
 from address.models import Address
+from reviews.models import Review
+from django.db.models import Avg
 
 class CafeSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     address1 = serializers.SerializerMethodField()
     address2 = serializers.SerializerMethodField()
+    ratings = serializers.SerializerMethodField()
     
     class Meta:
         model = Cafe
@@ -21,12 +24,23 @@ class CafeSerializer(serializers.ModelSerializer):
             'has_wall_outlets',
             'is_pet_friendly',
             'thumbnail_image_location',
+            'ratings'
         ]
     def get_address1(self, obj):
         return obj.address1.formatted
 
     def get_address2(self, obj):
         return obj.address2.formatted if obj.address2 else obj.address2
+    
+    def get_ratings(self, obj):
+        ratings = Review.objects.filter(cafe_id=obj.id).aggregate(
+            coffee_quality=Avg("coffee_quality"),
+            comfortability=Avg("comfortability"),
+            atmosphere=Avg("atmosphere"),
+            quietness=Avg("quietness"),
+            cleanliness=Avg("cleanliness"),
+        )
+        return ratings
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
@@ -34,3 +48,6 @@ class AddressSerializer(serializers.ModelSerializer):
         fields = [
             'formatted'
         ]
+
+# class RatingSerializer(serializers.ModelSerializer):
+    
